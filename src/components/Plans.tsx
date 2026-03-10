706a03bf23ce4fcba80da775d88dc747e3b13687
Olaniyi Olamide <olamideolaniyi616@gmail.com>
2026-03-10 14:26:01 +0100

    refactor:  recfactored the plans component to accept an active class fot the active plan.

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,13 +1,10 @@
-import ArcadeIcon from "../assets/icon-arcade.svg";
-import AdvancedIcon from "../assets/icon-advanced.svg";
-import ProIcon from "../assets/icon-pro.svg";
 import NextStepButton from "./NextStepButton";
 import GoBackButton from "./GoBackButton";
 import ToggleButton from "./ToggleButton";
 import { useForm } from "../context/FormContext";
 
 function Plans() {
-  const { monthly } = useForm();
+  const { monthly, plans, currentPlan, setCurrentPlan } = useForm();
 
   return (
     <section className="flex flex-col gap-[1rem] basis-[70%] px-[2rem] bg-Blue100 mx-[1.2rem] md:mx-0 py-[2rem] rounded-2xl mt-[-5rem] md:mt-0 mb-[2rem] md:mb-0">
@@ -21,59 +18,37 @@ function Plans() {
       </header>
 
       <div className="flex flex-col md:flex-row md:items-center gap-[1.5rem]">
-        <figure className="flex md:flex-col rounded-lg px-[0.8rem] py-[1rem] border-[0.12rem] border-Grey500 gap-[2rem] active-plan">
-          <img
-            src={ArcadeIcon}
-            alt="Arcade-Icon"
-            className="h-[2rem] w-[2rem]"
-          />
+        {plans.map(
+          (plan: { id: number; icon: string; plan: string; price: string }) => {
+            return (
+              <figure
+                key={plan.id}
+                className={`flex md:flex-col rounded-lg px-[0.8rem] py-[1rem] border-[0.12rem] border-Grey500 gap-[2rem] lg:w-[20%] ${
+                  currentPlan?.id === plan.id ? "active-plan" : ""
+                }`}
+                onClick={() => setCurrentPlan(plan)}
+              >
+                <img
+                  src={plan.icon}
+                  alt={plan.plan}
+                  className="h-[2rem] w-[2rem]"
+                />
 
-          <figcaption>
-            <h5 className="font-bold text-Blue950 text-[0.85rem]">Arcade</h5>
-            <p className="text-[0.75rem] text-Grey500">
-              {!monthly ? "$9/mo" : "$90/yr"}
-            </p>
-            {monthly && (
-              <p className="text-[0.75rem] text-Blue950 font-semibold">
-                2 months free
-              </p>
-            )}
-          </figcaption>
-        </figure>
-        <figure className="flex md:flex-col rounded-lg px-[0.8rem] py-[1rem] border-[0.12rem] border-Grey500 gap-[2rem]">
-          <img
-            src={AdvancedIcon}
-            alt="Advanced-Icon"
-            className="h-[2rem] w-[2rem]"
-          />
-
-          <figcaption>
-            <h5 className="font-bold text-Blue950 text-[0.85rem]">Advanced</h5>
-            <p className="text-[0.75rem] text-Grey500">
-              {!monthly ? "$12/mo" : "$120/yr"}
-            </p>
-            {monthly && (
-              <p className="text-[0.75rem] text-Blue950 font-semibold">
-                2 months free
-              </p>
-            )}
-          </figcaption>
-        </figure>
-        <figure className="flex md:flex-col rounded-lg px-[0.8rem] py-[1rem] border-[0.12rem] border-Grey500 gap-[2rem]">
-          <img src={ProIcon} alt="Pro-Icon" className="h-[2rem] w-[2rem]" />
-
-          <figcaption>
-            <h5 className="font-bold text-Blue950 text-[0.85rem]">Pro</h5>
-            <p className="text-[0.75rem] text-Grey500">
-              {!monthly ? "$15/mo" : "$150/yr"}
-            </p>
-            {monthly && (
-              <p className="text-[0.75rem] text-Blue950 font-semibold">
-                2 months free
-              </p>
-            )}
-          </figcaption>
-        </figure>
+                <figcaption>
+                  <h5 className="font-bold text-Blue950 text-[0.85rem]">
+                    {plan.plan}
+                  </h5>
+                  <p className="text-[0.75rem] text-Grey500">{plan.price}</p>
+                  {monthly && (
+                    <p className="text-[0.75rem] text-Blue950 font-semibold">
+                      2 months free
+                    </p>
+                  )}
+                </figcaption>
+              </figure>
+            );
+          }
+        )}
       </div>
 
       <div className="bg-Blue200 flex items-center justify-center w-full py-[1rem] rounded-lg gap-[1rem]">
--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,4 +1,7 @@
 import { createContext, useContext, useState } from "react";
+import ArcadeIcon from "../assets/icon-arcade.svg";
+import AdvancedIcon from "../assets/icon-advanced.svg";
+import ProIcon from "../assets/icon-pro.svg";
 
 interface Prop {
   children: React.ReactNode;
@@ -6,19 +9,56 @@ interface Prop {
 interface Context {
   monthly: boolean;
   handleToggle: () => void;
+  plans: Plan[];
+  currentPlan: Plan;
+  setCurrentPlan: React.Dispatch<React.SetStateAction<Plan>>;
+
+  //   currentPlan: ;
+  //   setCurrentPlan: React.Dispatch<React.SetStateAction<[]>>;
 }
+
+type Plan = {
+  id: number;
+  plan: string;
+  price: string;
+  icon: string;
+};
 
 const formContext = createContext<undefined | Context>(undefined);
 
 function FormProvider({ children }: Prop) {
   const [monthly, setMonthly] = useState(true);
+  const plans = [
+    {
+      id: 1,
+      plan: "Arcade",
+      price: !monthly ? "$9/mo" : "$90/yr",
+      icon: `${ArcadeIcon}`,
+    },
+    {
+      id: 2,
+      plan: "Advanced",
+      price: !monthly ? "$12/mo" : "$120/yr",
+      icon: `${AdvancedIcon}`,
+    },
+    {
+      id: 3,
+      plan: "Pro",
+      price: !monthly ? "$15/mo" : "$150/yr",
+      icon: `${ProIcon}`,
+    },
+  ];
+
+  const [currentPlan, setCurrentPlan] = useState<Plan>(plans[0]);
 
   function handleToggle() {
     setMonthly((m) => !m);
   }
 
   return (
-    <formContext.Provider value={{ monthly, handleToggle }}>
+    <formContext.Provider
+      value={{ monthly, handleToggle, plans, currentPlan, setCurrentPlan }}
+    >
       {children}
     </formContext.Provider>
   );
